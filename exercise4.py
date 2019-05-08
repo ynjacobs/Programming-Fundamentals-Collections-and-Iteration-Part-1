95a8d1826cf7842850df6fb5b5485296f45cda31
ynjacobs <ytj64611@gamil.com>
2019-05-08 15:40:50 -0400

    completed requirements for exercise 4

---
+++ exercise4.py
@@ -0,0 +1,17 @@
+from exercise0 import*
+
+for number in ages:
+    if number < 17:
+        print(number)
+
+print(max(ages))
+
+count = flip_coin.count('heads')
+print(count)
+
+
+fav_perf_artists.pop(2)
+print(fav_perf_artists)
+
+cities_dictionary['Toronto'] = 17000000
+print(cities_dictionary)
