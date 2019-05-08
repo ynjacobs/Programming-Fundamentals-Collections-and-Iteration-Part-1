5c0e06226efa5ebee742d9735a15197fef73f8e7
ynjacobs <ytj64611@gamil.com>
2019-05-08 14:45:39 -0400

    made lists and dictionaries

---
+++ exercise0.py
@@ -0,0 +1,29 @@
+fav_colours = ['blue', 'purple', 'silver']
+ages = [21, 19, 15, 3]
+flip_coin = ['heads', 'heads', 'heads', 'heads', 'heads']
+fav_perf_artists = ['Shwekey', 'Matt Dubb', '8th Day']
+
+words_dictionary = {
+  'alacrity': 'cheerful readiness',
+  'unwieldy': 'difficult to move',
+  'benevolence': 'kindness'
+
+}
+
+fav_movie_dictionary = {
+    'Lion King': 1994,
+    'Confessions of a Shopaholic': 2009,
+    'Back to the Future': 1984
+}
+
+cities_dictionary = {
+    'Toronto': 2930000,
+    'New York': 8623000,
+    'Tokyo': 9273000
+}
+
+names_ages_dictionary = {
+    'Tamara': 19,
+    'Serena': 15,
+    'Abby': 3
+}
