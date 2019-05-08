72a73ffda3beaeae6d5f2838fd530e3b3851b2d5
ynjacobs <ytj64611@gamil.com>
2019-05-08 15:07:59 -0400

    did exercise 2

---
+++ exercise2.py
@@ -0,0 +1,15 @@
+from exercise0 import*
+
+print(fav_colours[-1])
+
+cities_dictionary['L.A'] = 400000
+
+flip_coin.reverse()
+print(flip_coin)
+
+print(cities_dictionary['Toronto'])
+
+
+
+for name in fav_perf_artists:
+  print("{} is a great artist!".format(name))
