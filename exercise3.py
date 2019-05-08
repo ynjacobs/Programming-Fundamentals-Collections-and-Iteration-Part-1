dfb8391da1dcd3546458a67972f3ed7f78088085
ynjacobs <ytj64611@gamil.com>
2019-05-08 15:18:49 -0400

    completed requirements for exercise 3

---
+++ exercise3.py
@@ -0,0 +1,13 @@
+from exercise0 import*
+
+print(fav_perf_artists[0:2])
+
+for key, val in fav_movie_dictionary.items():
+  print("{} came out in {}".format(key, val))
+
+sorted(ages)
+ages.reverse()
+print(ages)
+
+fav_movie_dictionary['Beauty and the Beats'] = 1991, 2017
+print(fav_movie_dictionary)
