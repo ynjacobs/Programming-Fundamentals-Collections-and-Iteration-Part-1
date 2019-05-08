6a0b7f91d4ebd8dd5747d149c2715e510ae140cc
ynjacobs <ytj64611@gamil.com>
2019-05-08 17:04:46 -0400

    completed requirements for exercise 7

---
+++ exercise7.py
@@ -0,0 +1,43 @@
+# for i in range(20):
+#    print("I will not skateboard in the halls")
+
+# my_list = []
+
+# for i in range(20):
+#     my_list.append("I will not skateboard in the halls")
+    
+# print(my_list)
+
+
+# ur_list = list(range(1,51))
+# print(ur_list)
+
+
+# def sum_list(ur_list):
+#     sum = 0
+#     for x in ur_list:
+#         sum += x
+#     return sum
+# print(sum_list(ur_list))
+
+ur_list = []
+print(ur_list)
+
+def new_list():
+    for i in range(50):
+        for j in range(3):
+            ur_list.append(i+1)
+    
+
+new_list()
+print(ur_list)
+
+
+info_list = [
+    {'Name':'Canada', 'continent': 'NA', 'Island?': 'NO'} ,
+    {'Name':'U.S.A', 'continent': 'NA', 'Island?': 'NO'} ,
+    {'Name':'Mexico', 'continent': 'NA', 'Island?': 'NO'} 
+    
+]
+
+print(info_list)
