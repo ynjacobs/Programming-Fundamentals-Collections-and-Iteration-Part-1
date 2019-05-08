a2a84dfe805158a01d156961f5ba525d5577437a
ynjacobs <ytj64611@gamil.com>
2019-05-08 16:19:45 -0400

    completed requirements for exercise 6

---
+++ exercise6.py
@@ -0,0 +1,19 @@
+movies_for_each_year_dictionary = {
+  '1999': ['The Matrix', 'Star Wars: Episode 1', 'The Mummy'],
+  '2009': ['Avatar', 'Star Trek', 'District 9'],
+  '2019': ['How to Train Your Dragon 3', 'Toy Story 4', 'Star Wars: Episode 9']
+}
+
+new_list = [
+    [1,2,3], 
+    [4,5,5],
+    [7,8,9],
+    ['*',0,'#']
+]
+
+info_list = [
+    {'Name':'Canada', 'continent': 'NA', 'Island?': 'NO'} ,
+    {'Name':'U.S.A', 'continent': 'NA', 'Island?': 'NO'} ,
+    {'Name':'Mexico', 'continent': 'NA', 'Island?': 'NO'} 
+    
+]
